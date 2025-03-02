cec85091369312c40e2f49aa559ac630fa4ebab2
Kayla Altepeter <kayla.altepeter@datasite.com>
2025-03-02 15:22:06 -0600

    fix: poetry, install specific version (#41)

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 [[ ${DEBUG:-} == true ]] && set -o xtrace
-readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 PLATFORMS=${1:-linux/amd64,linux/arm64}
 DOCKER_REPO=${2:-0.0.0.0:5001}
@@ -13,5 +13,5 @@ echo "Building ${LOCAL_NAME}"
 
 docker buildx build --progress plain --no-cache --builder=container --platform="${PLATFORMS}" --push --pull -t "${LOCAL_NAME}:latest-python" "${__dir}/"
 
-docker buildx build --builder=container --platform="${PLATFORMS}" --push -t "${LOCAL_NAME}-nx-consumer:latest-python" --build-arg=DOCKER_REPO=${DOCKER_REPO} "${__dir}/test-nx"
-docker buildx build --builder=container --platform="${PLATFORMS}" --push -t "${LOCAL_NAME}-app-consumer:latest-python" --build-arg=DOCKER_REPO=${DOCKER_REPO} "${__dir}/test-app"
+docker buildx build --builder=container --platform="${PLATFORMS}" --push -t "${LOCAL_NAME}-nx-consumer:latest-python" --build-arg=DOCKER_REPO="${DOCKER_REPO}" "${__dir}/test-nx"
+docker buildx build --builder=container --platform="${PLATFORMS}" --push -t "${LOCAL_NAME}-app-consumer:latest-python" --build-arg=DOCKER_REPO="${DOCKER_REPO}" "${__dir}/test-app"
